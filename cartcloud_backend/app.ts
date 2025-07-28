c301ff48ce8ff1efb8a9b4398037eb992c8a006b
Caiya Varden <caiya.varden@ellefsontech.com>
2025-07-27 22:21:22 -0300

    Prometheus

--- cartcloud_backend/app.ts
+++ cartcloud_backend/app.ts
@@ -9,33 +9,39 @@ import cartRoutes from './routes/carts'
 import wishlistRoutes from './routes/wishlists'
 import orderRoutes from './routes/orders'
 import compression from 'compression';
-import helmet from 'helmet';
+import client from 'prom-client';
 
 
 const env = process.env.NODE_ENV || 'dev';
 dotenv.config({ path: `.env.${env}` });
 
 const app = express();
+
+client.collectDefaultMetrics();
+
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [0.1, 0.5, 1, 1.5, 2, 5] // Customize as needed
+});
+
+
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.route?.path || req.path, status_code: res.statusCode });
+  });
+  next();
+});
+
 app.use(express.json());
-app.use(cors())
+app.options('*', cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(compression());
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      useDefaults: true,
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'"],
-        styleSrc: ["'self'"],
-        imgSrc: ["'self'", "data:"],
-        fontSrc: ["'self'"],
-        connectSrc: ["'self'"],
-        frameAncestors: ["'none'"],
-      },
-    },
-    frameguard: { action: 'deny' },
-  })
-);
+
 
 app.use('/users', userRoutes)
 app.use('/products', productRoutes)
@@ -48,6 +54,11 @@ connectDB();
 
 app.get('/', (_req, res) => {
   res.send('API is running...');
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
 const PORT = process.env.PORT || 3000;
